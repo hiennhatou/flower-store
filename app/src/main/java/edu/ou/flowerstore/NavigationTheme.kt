1e0249058aa9049d2ecb0748af2bbdd2836743d5
hiennhatou <2251012058hien@ou.edu.vn>
2024-11-04 10:16:37 +0700

    fix bottom navigation

--- app/src/main/java/edu/ou/flowerstore/NavigationTheme.kt
+++ app/src/main/java/edu/ou/flowerstore/NavigationTheme.kt
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class navigation_theme : AppCompatActivity() {
+class NavigationTheme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_navigation_theme)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation_theme)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
